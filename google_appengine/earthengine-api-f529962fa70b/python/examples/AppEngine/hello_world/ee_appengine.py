803ba98eafdee0f102a951f4a5614d8d678c4d65
mst-cfg <mbellamy@mills.edu>
2015-05-14 20:16:01 -0700

    updated comments may 14

--- google_appengine/earthengine-api-f529962fa70b/python/examples/AppEngine/hello_world/ee_appengine.py
+++ google_appengine/earthengine-api-f529962fa70b/python/examples/AppEngine/hello_world/ee_appengine.py
@@ -8,11 +8,11 @@
 # instructions on how to set up authentication.
 
 import os
-
 import config
 import ee
 import jinja2
 import webapp2
+import json
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -20,16 +20,32 @@ jinja_environment = jinja2.Environment(
 
 class MainPage(webapp2.RequestHandler):
   def get(self):                             # pylint: disable=g-bad-name
-    """Request an image from Earth Engine and render it to a web page."""
-    ee.Initialize(config.EE_CREDENTIALS, config.EE_URL)
-    mapid = ee.Image('srtm90_v4').getMapId({'min': 0, 'max': 1000})
+    """Request an ImageCollection from Earth Engine filtered by date and render it to a web page."""
+    ee.Initialize(config.EE_CREDENTIALS, config.EE_URL)		# logs in as credentialed appspot user
+    imageCollection = ee.ImageCollection("LANDSAT/LE7_L1T")	# imports Landsat 7 Raw Scenes as desired image type
+    geometry = ee.Geometry.Polygon(				# imports the Polygon class so user can hard-code four-cornered shape for image collection queries
+	[[[-97.03125, 27.963354676702952],
+	  [-82.44140625, 28.273423646227233],
+	  [-83.84765625, 30.266658976560755],
+	  [-97.91015625, 30.1907194801663]]])
+    myImageCollection = imageCollection.filterBounds(geometry).filterDate('2014-01-01', '2014-03-31')	# hard-coded duration of time from which images should be extracted.
+    coll = myImageCollection.getInfo()									# sets variable as the properties of the images pulled
+    result = []												# initialized empty list to populate with image IDs
+    for each in range(0, len(coll)):
+	imageID = coll["features"][each]["id"]								# variable holding each of the LS7 image's unique ID
+	result.append(imageID)
+    list_ = result											# declare list variable that includes unicode character in generated list
+    list_as_json = json.dumps(list_)									# get rid of the unicode character
+    print list_as_json											# print the list of image IDs without the unicode character
+	
+
+
 
     # These could be put directly into template.render, but it
     # helps make the script more readable to pull them out here, especially
     # if this is expanded to include more variables.
     template_values = {
-        'mapid': mapid['mapid'],
-        'token': mapid['token']
+        'myList': list_as_json					# sends clean image ID list to index.html file to be rendered on a web page once app is deployed
     }
     template = jinja_environment.get_template('index.html')
     self.response.out.write(template.render(template_values))
